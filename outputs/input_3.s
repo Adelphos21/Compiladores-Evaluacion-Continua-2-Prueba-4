9470a54ef4d4cc04508b9a4499fb107274b9100f
Matias Sanchez <82473529+Adelphos21@users.noreply.github.com>
2025-11-20 14:35:47 -0500

    Add files via upload

---
+++ outputs/input_3.s
@@ -0,0 +1,29 @@
+.data
+print_fmt: .string "%ld \n"
+.text
+.globl main
+main:
+ pushq %rbp
+ movq %rsp, %rbp
+ subq $8, %rsp
+ movq $1, %rax
+ cmpq $0, %rax
+ je else_0
+ movq $1, %rax
+ movq %rax, -8(%rbp)
+ jmp endif_0
+ else_0:
+ movq $2, %rax
+ movq %rax, -8(%rbp)
+endif_0:
+ movq -8(%rbp), %rax
+ movq %rax, %rsi
+ leaq print_fmt(%rip), %rdi
+ movl $0, %eax
+ call printf@PLT
+ movq $0, %rax
+ jmp .end_main
+.end_main:
+leave
+ret
+.section .note.GNU-stack,"",@progbits
